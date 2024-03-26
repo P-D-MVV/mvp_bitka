43ae0f049d2050d8ee3549007d3aeb18e7a30b7b
gabrielpereiravaleverde <gabriel.pereira@vale-verde.com>
2024-03-26 09:55:58 -0300

    alteração no launcher(adição dos install.packages)

--- app/db/functions/rodar_consulta.py
+++ app/db/functions/rodar_consulta.py
@@ -2,4 +2,9 @@ import subprocess
 import os
 
 def rodar():
+    os.system('Rscript install.packages("dplyr")')
+    os.system('Rscript install.packages("readxl")')
+    os.system('Rscript install.packages("writexl")')
+    os.system('Rscript install.packages("DBI")')
+    os.system('Rscript install.packages("odbc")')
     os.system("Rscript app/db/functions/coleta_de_dados.R")
