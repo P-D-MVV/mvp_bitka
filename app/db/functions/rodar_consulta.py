9f62da8e40b04ebceaf4ff24814cb5fda17c4e8c
gabrielpereiravaleverde <gabriel.pereira@vale-verde.com>
2024-03-26 11:28:29 -0300

    teste de subprocess ao invés de os

--- app/db/functions/rodar_consulta.py
+++ app/db/functions/rodar_consulta.py
@@ -2,9 +2,15 @@ import subprocess
 import os
 
 def rodar():
-    os.system('Rscript install.packages("dplyr")')
-    os.system('Rscript install.packages("readxl")')
-    os.system('Rscript install.packages("writexl")')
-    os.system('Rscript install.packages("DBI")')
-    os.system('Rscript install.packages("odbc")')
-    os.system("Rscript app/db/functions/coleta_de_dados.R")
+    # os.system('Rscript install.packages("dplyr")')
+    # os.system('Rscript install.packages("readxl")')
+    # os.system('Rscript install.packages("writexl")')
+    # os.system('Rscript install.packages("DBI")')
+    # os.system('Rscript install.packages("odbc")')
+    # os.system("Rscript app/db/functions/coleta_de_dados.R")
+    subprocess.run(["Rscript", 'install.packages("dplyr")'])
+    subprocess.run(["Rscript", 'install.packages("readxl")'])
+    subprocess.run(["Rscript", 'install.packages("writexl")'])
+    subprocess.run(["Rscript", 'install.packages("DBI")'])
+    subprocess.run(["Rscript", 'install.packages("odbc")'])
+    subprocess.run(["Rscript", 'app/db/functions/coleta_de_dados.R'])
